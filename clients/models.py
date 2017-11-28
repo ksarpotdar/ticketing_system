e86cecb8118fbbc6c22a19399fb67561c40fd322
Marisela Del Valle <marisela.delvalle93@gmail.com>
2017-11-28 00:38:18 -0400

    tickets: renaming fields and adding status field

--- clients/models.py
+++ clients/models.py
@@ -10,12 +10,17 @@ class TicketModel(models.Model):
     name = models.CharField(max_length=100, blank=False)
     email = models.EmailField(max_length=100, blank=False)
     subject = models.CharField(max_length=100, blank=False)
-    urgency = models.CharField(max_length=50, blank=False,
+    ticket_type = models.CharField(max_length=50, blank=False,
                                choices=[('Bug Report', 'Bug Report'),
                                         ('Feature Request', 'Feature Request'),
-                                        ('Other', 'Other')],)
-    ticket_type = models.CharField(max_length=50, blank=False,
+                                        ('Other', 'Other')])
+    urgency = models.CharField(max_length=50, blank=False,
                                    choices=[('Low', 'Low'),
                                             ('Mid', 'Mid'),
                                             ('High', 'High')])
     message = models.TextField(max_length=9999)
+    status = models.CharField(max_length=50, default="Open",
+                                   choices=[('Open', 'Open'),
+                                            ('In Progress', 'In Progress'),
+                                            ('Completed', 'Completed'),
+                                            ('Rejected', 'Rejected')])
